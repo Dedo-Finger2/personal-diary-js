dd15b85f2ba79033cca27dedee1d0cab99fe1b04
antonio mauricio <antonioimportant@gmail.com>
2024-12-04 10:12:08 -0300

    fix: iv conversion to and from base64 returning a iv with only 1 bye

--- security.js
+++ security.js
@@ -1,19 +1,14 @@
 export async function convertKeyAndIVToBase64(key, iv) {
   const rawKey = await crypto.subtle.exportKey("raw", key);
-
   const keyBase64 = btoa(String.fromCharCode(...new Uint8Array(rawKey)));
-  const ivBase64 = btoa(String.fromCharCode(iv));
-
-  return {
-    keyBase64,
-    ivBase64,
-  };
+  const ivBase64 = btoa(String.fromCharCode(...iv));
+  return { keyBase64, ivBase64 };
 }
 
 export async function getKeyAndIVFromLocalStorage() {
   const cryptoKeys = JSON.parse(localStorage.getItem("encryptionKeys"));
 
-  if (!cryptoKeys.keyBase64 || !cryptoKeys.ivBase64) {
+  if (!cryptoKeys || !cryptoKeys.keyBase64 || !cryptoKeys.ivBase64) {
     throw new Error("AES Key or IV not found.");
   }
 
@@ -21,6 +16,10 @@ export async function getKeyAndIVFromLocalStorage() {
     c.charCodeAt(0),
   );
   const iv = Uint8Array.from(atob(cryptoKeys.ivBase64), (c) => c.charCodeAt(0));
+
+  if (iv.length !== 12) {
+    throw new Error("Invalid IV length. Expected 12 bytes.");
+  }
 
   const aesKey = await crypto.subtle.importKey(
     "raw",
@@ -37,7 +36,7 @@ function arrayBufferToBase64(buffer) {
   let binary = "";
   const bytes = new Uint8Array(buffer);
   const len = bytes.length;
-  for (let index = 0; index <= len; index++) {
+  for (let index = 0; index < len; index++) {
     binary += String.fromCharCode(bytes[index]);
   }
   return btoa(binary);
@@ -47,10 +46,7 @@ export async function encryptData(key, iv, data) {
   const encoder = new TextEncoder();
   const encodedData = encoder.encode(data);
   const encryptedData = await crypto.subtle.encrypt(
-    {
-      name: "AES-GCM",
-      iv: iv,
-    },
+    { name: "AES-GCM", iv: iv },
     key,
     encodedData,
   );
@@ -67,10 +63,7 @@ export async function decryptData(key, iv, encryptedData) {
   }
   const arrayBuffer = bytes.buffer;
   const decryptedBuffer = await crypto.subtle.decrypt(
-    {
-      name: "AES-GCM",
-      iv: iv,
-    },
+    { name: "AES-GCM", iv: iv },
     key,
     arrayBuffer,
   );
@@ -81,10 +74,7 @@ export async function decryptData(key, iv, encryptedData) {
 
 export async function getCryptoKey() {
   const aesKey = await crypto.subtle.generateKey(
-    {
-      name: "AES-GCM",
-      length: 256,
-    },
+    { name: "AES-GCM", length: 256 },
     true,
     ["encrypt", "decrypt"],
   );
