9556cba4520a890c8bd86c5d6fc0c5f27436e9b5
antonio mauricio <antonioimportant@gmail.com>
2024-12-04 08:48:21 -0300

    feat: adds saving and security for the github api key + adds components file

---
+++ components.js
@@ -0,0 +1,15 @@
+const SettingsForm = document.querySelector("#entry-settings-form");
+const ApiKeyInput = document.querySelector("#api-key");
+const RepositoryPlataformSelect = document.querySelector(
+  "#repository-plataform",
+);
+const RepositoryNameInput = document.querySelector("#repository-name");
+const BranchNameInput = document.querySelector("#branch-name");
+
+export {
+  SettingsForm,
+  ApiKeyInput,
+  RepositoryNameInput,
+  RepositoryPlataformSelect,
+  BranchNameInput,
+};
