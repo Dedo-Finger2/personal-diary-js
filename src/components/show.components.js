1aedddb49accb03c1b7f3f2ee77836adc5b10f05
antonio mauricio <antonioimportant@gmail.com>
2024-12-05 08:06:59 -0300

    feat: adds show.html page and it's components

---
+++ src/components/show.components.js
@@ -0,0 +1,7 @@
+const DiaryEntryTitleH1 = document.querySelector("#diary-entry-title");
+const DiaryEntryCategoriesDiv = document.querySelector(
+  "#diary-entry-categories",
+);
+const DiaryEntryBodyTextarea = document.querySelector("#diary-entry-body");
+
+export { DiaryEntryTitleH1, DiaryEntryCategoriesDiv, DiaryEntryBodyTextarea };
