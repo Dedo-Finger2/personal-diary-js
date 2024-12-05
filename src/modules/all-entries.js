ffcff97b37cc3409710521c057b2bfbf1826ac7f
antonio mauricio <antonioimportant@gmail.com>
2024-12-05 15:32:27 -0300

    feat: makes all action buttons in listing table have also the sha of the file

--- src/modules/all-entries.js
+++ src/modules/all-entries.js
@@ -1,4 +1,11 @@
-import { EntriesTable } from "./../components/all-diary-entries.components.js";
+import {
+  EntriesTable,
+  ConfirmActionModalBodyParagraph,
+  ConfirmActionModalDialog,
+  ConfirmActionModalConfirmButton,
+  ConfirmActionModalCancelButton,
+  ConfirmActionModalTitleSpan,
+} from "./../components/all-diary-entries.components.js";
 import { getAllFiles } from "./../model/repository.js";
 
 async function populateTable() {
@@ -24,6 +31,7 @@ async function populateTable() {
     viewBtn.href = `show.html?path=${file.path}`;
     viewBtn.textContent = "View";
     viewBtn.setAttribute("file-path", file.path);
+    viewBtn.setAttribute("sha", file.sha);
 
     const editBtn = document.createElement("a");
     editBtn.classList.add("entry-action-btn");
@@ -31,12 +39,14 @@ async function populateTable() {
     editBtn.href = `edit.html?path=${file.path}`;
     editBtn.textContent = "Edit";
     editBtn.setAttribute("file-path", file.path);
+    editBtn.setAttribute("sha", file.sha);
 
     const deleteBtn = document.createElement("a");
     deleteBtn.classList.add("entry-action-btn");
     deleteBtn.id = "delete-diary-entry";
     deleteBtn.textContent = "Delete";
     deleteBtn.setAttribute("file-path", file.path);
+    deleteBtn.setAttribute("sha", file.sha);
 
     actionsTd.appendChild(viewBtn);
     actionsTd.appendChild(editBtn);
@@ -51,6 +61,39 @@ async function populateTable() {
   }
 }
 
+function handleDeleteDiaryentry() {
+  ConfirmActionModalTitleSpan.textContent =
+    "Are you sure you want to delete this diary entry?";
+  ConfirmActionModalBodyParagraph.textContent =
+    "This action is not reversable!";
+
+  ConfirmActionModalDialog.showModal();
+
+  ConfirmActionModalDialog.setAttribute("purpose", "DELETE-DIARY-ENTRY");
+}
+
 (async () => {
   await populateTable();
+
+  // Needs to be here cause the buttons are created by JS
+  const DeleteDiaryEntryButton = document.querySelector("#delete-diary-entry");
+  const ViewDiaryEntryButton = document.querySelector("#view-diary-entry");
+  const EditDiaryEntryButton = document.querySelector("#edit-diary-entry");
+
+  DeleteDiaryEntryButton.addEventListener("click", handleDeleteDiaryentry);
+
+  ConfirmActionModalConfirmButton.addEventListener("click", () => {
+    const dialogPurpose = ConfirmActionModalDialog.getAttribute("purpose");
+    switch (dialogPurpose) {
+      case "DELETE-DIARY-ENTRY":
+        console.log("Eba");
+        break;
+      default:
+        alert("Something went wrong!");
+    }
+  });
 })();
+
+ConfirmActionModalCancelButton.addEventListener("click", () =>
+  ConfirmActionModalDialog.close(),
+);
